7c7246cfb0ffe4c8d49d1b3627a1bd4e3d96e941
Yongtao Hu <herohuyongtao@gmail.com>
2015-12-30 10:14:03 +0800

    add Native C++ SDK; re-org files

---
+++ Native C++/X-Hawk.h
@@ -0,0 +1,61 @@
+#ifndef __XIM_HAWK_H__
+#define __XIM_HAWK_H__
+
+#define XIM_API __declspec(dllexport)
+
+#define ERROR_NO_DEVICE -1
+
+#define MASK_NOT_FOUND       0x0
+#define MASK_POSITION_FOUND  0x1
+#define MASK_ROTATION_FOUND  0x2
+#define MASK_POSE_FOUND      0x3
+
+#define MAX_BUTTONS          0x5
+
+typedef struct JOYINFO_TAG
+{
+  /** Flags **/
+  unsigned char  id;              // 0 for left-hand joystick, 1 for right-hand one
+  unsigned char  found_mask;      // flag of {MASK_NOT_FOUND, MASK_POSITION_FOUND, MASK_ROTATION_FOUND, MASK_POSE_FOUND}
+
+  /** Gamepad **/
+  float          joystick_x;      // value of x-axis
+  float          joystick_y;      // value of y-axis
+  float          trigger;         // trigger value
+  int            buttons;         // button mask
+  
+  /** Motion **/
+  float          position[3];     // 3d position of joystick
+  float          rotation[4];     // rotation of joystick
+  float          eulerAngles[3];  // euler angles of joystick
+} JOYINFO;
+
+typedef void (*void_delegate)();
+
+/** Common **/
+extern "C" XIM_API int XHawkInit(void);
+extern "C" XIM_API int XHawkExit(void);
+
+/** Read **/
+// Get the number of joysticks
+extern "C" XIM_API int XHawkGetJoystickCount(void);
+// Get a joystick info from read cache
+extern "C" XIM_API int XHawkGetJoystick(int index, JOYINFO *joystick);
+// Get all joystick infos
+extern "C" XIM_API int XHawkGetAllJoysticks(JOYINFO *joysticks);
+// Get a newest joystick info from read cache
+extern "C" XIM_API int XHawkGetNewestJoystick(int index, JOYINFO *joystick);
+// Get all newest joystick infos
+extern "C" XIM_API int XHawkGetAllNewestJoysticks(JOYINFO *joysticks);
+
+/** Write **/
+// send message to joystick, reserved, not implemented yet
+extern "C" XIM_API int XHawkSendMessage(int index, int Msg, int wParam, int lParam);
+
+/** Events **/
+// set a read listener, if you use this function, use XHawkGetJoystick or XHawkGetAllJoysticks on callback function
+extern "C" XIM_API int XHawkSetReadListener(void_delegate read_callback);
+// read the first frame if true, read last frame if false
+extern "C" XIM_API int XHawkSetReadFirstFrame(bool is_first_frame);
+
+#endif
---
+++ Native C++/test.cpp
@@ -0,0 +1,49 @@
+#include "X-Hawk.h"
+
+#include <iostream>
+
+int main()
+{
+  // init X-Hawk
+  XHawkInit();
+
+  // get all joysticks
+  JOYINFO joysticks[2];
+  XHawkGetAllNewestJoysticks(joysticks);
+
+  // print info of joysticks
+  for (int i = 0; i < 2; ++i) {
+    std::cout << "---> Info of Joystick #" << i << " <---\n";
+
+    std::cout << "\t Flag info:\n";
+    std::cout << "\t\t Id: " << (int)joysticks[i].id << "\n";
+    std::cout << "\t\t Found_mask: " << (int)joysticks[i].found_mask << "\n";
+
+    std::cout << "\t Gamepad info:\n";
+    std::cout << "\t\t Position of joystick: (" << joysticks[i].joystick_x << ", " << joysticks[i].joystick_y << ")\n";
+    std::cout << "\t\t Trigger: " << joysticks[i].trigger << "\n";
+    std::cout << "\t\t Buttons: " << joysticks[i].buttons << "\n";
+
+    std::cout << "\t Motion info:\n";
+    std::cout << "\t\t Position: (" 
+              << joysticks[i].position[0] << ", " 
+              << joysticks[i].position[1] << ", " 
+              << joysticks[i].position[2] << ")\n";
+    std::cout << "\t\t Rotation: (" 
+              << joysticks[i].rotation[0] << ", " 
+              << joysticks[i].rotation[1] << ", " 
+              << joysticks[i].rotation[2] << ", " 
+              << joysticks[i].rotation[3] << ")\n";
+    std::cout << "\t\t Euler angles: (" 
+              << joysticks[i].eulerAngles[0] << ", " 
+              << joysticks[i].eulerAngles[1] << ", " 
+              << joysticks[i].eulerAngles[2] << ")\n";
+
+    std::cout << "\n";
+  }
+
+  // exit X-Hawk
+  XHawkExit();
+
+  return 0;
+}
